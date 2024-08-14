f2aa061b307b3a9625d6c5739a4a8074c8c4d769
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2024-08-15 00:46:15 +0530

    created models for database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+dotenv.config();
+const app=express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgon("common"));
+app.use(bodyParser.json({limit:"30mb" ,extended :true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
